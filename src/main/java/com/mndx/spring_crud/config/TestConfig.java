f43e0e63782452cb957b3a7d6e7a77efa39696e6
Felipe Mendes <lfelipecmendes@icloud.com>
2025-02-18 17:44:52 -0300

    feat: add `OrderStatus` class and `orderStatus` property on `Order` entity

--- src/main/java/com/mndx/spring_crud/config/TestConfig.java
+++ src/main/java/com/mndx/spring_crud/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.mndx.spring_crud.config;
 
 import com.mndx.spring_crud.entities.Order;
 import com.mndx.spring_crud.entities.User;
+import com.mndx.spring_crud.entities.enums.OrderStatus;
 import com.mndx.spring_crud.repositories.OrderRepository;
 import com.mndx.spring_crud.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,9 @@ public class TestConfig implements CommandLineRunner {
 				User userOne = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 				User userTwo = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-				Order orderOne = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), userOne);
-				Order orderTwo = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), userTwo);
-				Order orderThree = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), userOne);
+				Order orderOne = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), userOne, OrderStatus.PAID);
+				Order orderTwo = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), userTwo, OrderStatus.WAITING_PAYMENT);
+				Order orderThree = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), userOne, OrderStatus.WAITING_PAYMENT);
 
 				userRepository.saveAll(Arrays.asList(userOne, userTwo));
 				orderRepository.saveAll(Arrays.asList(orderOne, orderTwo, orderThree));
--- src/main/java/com/mndx/spring_crud/entities/Order.java
+++ src/main/java/com/mndx/spring_crud/entities/Order.java
@@ -1,6 +1,7 @@
 package com.mndx.spring_crud.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mndx.spring_crud.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -22,14 +23,16 @@ public class Order implements Serializable {
 		@ManyToOne
 		@JoinColumn(name = "client_id")
 		private User client;
+		private Integer orderStatus;
 
 		public Order() {
 		}
 
-		public Order(Long id, Instant moment, User client) {
+		public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
 				this.id = id;
 				this.moment = moment;
 				this.client = client;
+				setOrderStatus(orderStatus);
 		}
 
 		public Long getId() {
@@ -54,6 +57,14 @@ public class Order implements Serializable {
 
 		public void setClient(User client) {
 				this.client = client;
+		}
+
+		public OrderStatus getOrderStatus() {
+				return OrderStatus.valueOf(orderStatus);
+		}
+
+		public void setOrderStatus(OrderStatus orderStatus) {
+				this.orderStatus = orderStatus.getCode();
 		}
 
 		@Override
