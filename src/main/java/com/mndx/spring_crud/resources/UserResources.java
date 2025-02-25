18696d839f72771f3b2e0a33ca219c40a9585db7
Felipe Mendes <lfelipecmendes@icloud.com>
2025-02-25 13:49:24 -0300

    feat: add ability to insert users

--- src/main/java/com/mndx/spring_crud/resources/UserResources.java
+++ src/main/java/com/mndx/spring_crud/resources/UserResources.java
@@ -4,11 +4,10 @@ import com.mndx.spring_crud.entities.User;
 import com.mndx.spring_crud.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,5 +32,17 @@ public class UserResources {
 				}
 
 				return ResponseEntity.ok().body(user);
+		}
+
+		@PostMapping
+		public ResponseEntity<User> insertUser(@RequestBody User user) {
+				user = userService.insert(user);
+				URI uri = ServletUriComponentsBuilder
+								.fromCurrentRequest()
+								.path("/{id}")
+								.buildAndExpand(user.getId())
+								.toUri();
+
+				return ResponseEntity.created(uri).body(user);
 		}
 }
--- src/main/java/com/mndx/spring_crud/services/UserService.java
+++ src/main/java/com/mndx/spring_crud/services/UserService.java
@@ -20,4 +20,8 @@ public class UserService {
 		public Optional<User> findById(Long id) {
 				return userRepository.findById(id);
 		}
+
+		public User insert(User user) {
+				return userRepository.save(user);
+		}
 }
